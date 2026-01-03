916344a17896e06a0f93f2eb224359e30877d491
Omar Rahman <omar@r4men.com>
2026-01-03 07:45:06 -0800

    Add Mod type definition to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,10 @@
 export const mModId: string = 'FMcIBFPB';
 export const cModId: string = '1399042';
+
+export type Mod = {
+  name: string;
+  gitHubOwner: string;
+  gitHubRepo: string;
+  modrinthId?: string;
+  curseforgeId?: string;
+};
