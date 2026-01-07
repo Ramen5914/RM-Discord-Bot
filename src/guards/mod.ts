a65b6db88a19249756fc28e62bea21397c6fc371
Omar Rahman <omar@r4men.com>
2026-01-06 20:38:26 -0800

    Comment out IsMod guard function and related code in mod.ts

--- src/guards/mod.ts
+++ src/guards/mod.ts
@@ -1,23 +1,19 @@
-import { CommandInteraction, GuildMember, PermissionFlagsBits } from 'discord.js';
-import { GuardFunction } from 'discordx';
-import { db } from '../main.js';
+// export const IsMod: GuardFunction<CommandInteraction> = async (params, client, next, data) => {
+//   const modRoleId = db.get('modRoleId');
 
-export const IsMod: GuardFunction<CommandInteraction> = async (params, client, next, data) => {
-  const modRoleId = db.get('modRoleId');
+//   const member = params.member as GuildMember | null;
+//   if (!member) {
+//     data.guardPassed = false;
+//     return;
+//   }
 
-  const member = params.member as GuildMember | null;
-  if (!member) {
-    data.guardPassed = false;
-    return;
-  }
+//   const roleArray = [...member.roles.cache.keys()];
+//   const passed =
+//     roleArray.includes(modRoleId) || member.permissions.has(PermissionFlagsBits.Administrator);
 
-  const roleArray = [...member.roles.cache.keys()];
-  const passed =
-    roleArray.includes(modRoleId) || member.permissions.has(PermissionFlagsBits.Administrator);
+//   data.guardPassed = passed;
 
-  data.guardPassed = passed;
-
-  if (passed) {
-    await next();
-  }
-};
+//   if (passed) {
+//     await next();
+//   }
+// };
