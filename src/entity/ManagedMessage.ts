62aa53d3cd496079009d52162d2b281924bc3d29
Omar Rahman <omar@r4men.com>
2026-01-12 14:03:25 -0800

    Add ManagedMessageEntity with MessageType enum and relationships

---
+++ src/entity/ManagedMessage.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+import { ModEntity } from './Mod.js';
+
+export enum MessageType {
+  DOWNLOAD_INFO = 'downloadInfo',
+}
+
+@Entity('managed_message')
+export class ManagedMessageEntity {
+  @PrimaryColumn({ type: 'varchar', length: 19 })
+  id: string;
+
+  @Column({
+    type: 'enum',
+    enum: MessageType,
+  })
+  type: MessageType;
+
+  @ManyToOne(() => ModEntity, (mod) => mod.managedMessages)
+  mod: ModEntity;
+}
